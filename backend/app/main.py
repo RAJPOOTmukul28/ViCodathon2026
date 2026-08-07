c92818d80d5c1e07e12fd8a1b3771623266af88a
Mukul Rajpoot <rajpootmukul28@gmail.com>
2026-08-07 20:20:41 +0530

    Backend Gemini API working

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from app.services.gemini_service import ask_gemini
 
 app = FastAPI(title="ViCodathon API")
 
+
+class Prompt(BaseModel):
+    prompt: str
+
+
 @app.get("/")
 def home():
+    return {"message": "ViCodathon API Running 🚀"}
+
+
+@app.post("/chat")
+def chat(data: Prompt):
     return {
-        "message": "ViCodathon API Running 🚀"
+        "response": ask_gemini(data.prompt)
     }
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
