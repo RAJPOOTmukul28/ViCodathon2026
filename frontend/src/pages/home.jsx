2700ebe623e53e5b1a3c5da8c9fdeea76a038711
Mukul Rajpoot <rajpootmukul28@gmail.com>
2026-08-07 07:29:29 +0530

    Hackathon ready

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <h1>🚀 ViCodathon 2026</h1>
+      <p>Hackathon Starter Template Ready</p>
+    </div>
+  );
+}
